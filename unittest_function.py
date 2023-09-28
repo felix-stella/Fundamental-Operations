69b50b3dab94812c0cb0803a0815884178ad07aa
86138 <86138@DESKTOP-VJ7KAE6>
2023-09-28 13:38:39 +0800

    更改function文件中的拼写错误，增加Fraction,Expression类的单元测试和测试数据文件

--- unittest_function.py
+++ unittest_function.py
@@ -1,5 +1,5 @@
 import unittest
-from function import Number
+from function import Number,Fraction,Expression
 
 def read_test_data(filename):
     with open(filename, 'r') as file:
@@ -56,7 +56,58 @@ class TestNumber(unittest.TestCase):
         except ValueError as e:
             self.assertEqual(str(e), "max must be an integer and max must not be less than 1.")
 
+class TestFractionClass(unittest.TestCase):
 
+    def test_valid_initialization(self):
+        test_data = read_test_data('test_data/tests_calculate.txt')
+        expected_results = read_test_data('test_data/expected_results_calculate.txt')
+        for i, test_case in enumerate(test_data):
+            nums = test_case.split(',')
+            num_1=Number(nums=(int(nums[0]),int(nums[1]),int(nums[2])))
+            num_2=Number(nums=(int(nums[4]),int(nums[5]),int(nums[6])))
+            op=nums[3]
+            f=Fraction(num_1,num_2,op)
+            result = str(f.calculate_fractions())
+            expected_result = expected_results[i]
+            self.assertEqual(result, expected_result)
+
+    def test_subexpression_is_zero(self):
+        num_1 = Number(nums=(1, 2, 3))
+        num_2 = Number(nums=(0, 0, 3))
+        f=Fraction(num_1,num_2,'%')
+        try:
+            result=f.calculate_fractions()
+        except ValueError as e:
+            self.assertEqual(str(e), "The divisor cannot be 0.")
+
+class TestExpressionClass(unittest.TestCase):
+
+    def test_calculate_answer(self):
+        expression=Expression(10,10)
+        exp_list_1=[[Number(nums=(0,3,2)),'+',Number(nums=(1,0,2))],['*',Number(nums=(5,0,2))],
+                    ['%',Number(nums=(2,0,2))]]
+        result_1=expression.calculate_answer(exp_list_1)
+        expected_result_1=Number(nums=(6,1,4))
+        self.assertEqual(str(result_1), str(expected_result_1))
+
+        exp_list_2=[[Number(nums=(1,1,2)),'*',Number(nums=(1,3,2))],['%',Number(nums=(2,0,2))]]
+        result_2=expression.calculate_answer(exp_list_2)
+        expected_result_2=Number(nums=(1,7,8))
+        self.assertEqual(str(result_2), str(expected_result_2))
+
+        exp_list_3=[[Number(nums=(0,3,2)),'-',Number(nums=(1,0,2))],['*',Number(nums=(5,0,2))],
+                    ['%',Number(nums=(2,0,2))]]
+        try:
+            result_3=expression.calculate_answer(exp_list_3)
+        except ValueError as e:
+            self.assertEqual(str(e),"Sub expression cannot be less than zero.")
+
+        exp_list_4=[[Number(nums=(1,3,2)),'-',Number(nums=(1,0,2))],['-',Number(nums=(5,0,2))],
+                    ['%',Number(nums=(2,0,2))]]
+        try:
+            result_4=expression.calculate_answer(exp_list_4)
+        except ValueError as e:
+            self.assertEqual(str(e),"Sub expression cannot be less than zero.")
 
 if __name__ == '__main__':
     unittest.main()
